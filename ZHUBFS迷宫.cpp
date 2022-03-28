d10a871e3858b2b949efd75073e86505f7a24b57
Lsee47 <97860395+Lsee47@users.noreply.github.com>
2022-03-28 17:32:29 +0800

    Add files via upload

---
+++ ZHUBFS迷宫.cpp
@@ -0,0 +1,69 @@
+﻿#include<iostream>
+#include<stdlib.h>
+using namespace std;
+
+//@file		:ZHUBFS迷宫
+//@author	:Lvsq
+//@time		:2022/3/22 16:49:35
+
+const int N = 110;//地图最大范围
+int dx[4] = { -1,0,1,0 };//x移动范围
+int dy[4] = { 0,1,0,-1 };//y移动范围
+int q[100000];//数组队列
+int front = 0;//队列首指针
+int rear = 0;//队列尾指针
+
+int BFS(int map[N][N], int vis[N][N], int dist[N][N], int n, int m, int sx, int sy) {
+	front = rear = 0;//清空队列
+	q[rear++] = sx;
+	q[rear++] = sy;//起点入队
+	dist[sx][sy] = 0;
+	vis[sx][sy] = 1;
+	//队列非空
+	while (front<rear) {
+		int x = q[front++];
+		int y = q[front++];
+		for (int i = 0; i < 4; i++) {
+			//周围的点
+			int nx = x + dx[i];
+			int ny = y + dy[i];
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m || vis[nx][ny] == 1 || map[nx][ny] == 1) {
+				continue;
+			}
+			if (map[nx][ny] == 4) {
+				return dist[x][y] + 1;
+			}
+			q[rear++] = nx;
+			q[rear++] = ny;
+			vis[nx][ny] = 1;
+			dist[nx][ny] = dist[x][y] + 1;
+		}
+	}
+	return -1;
+}
+int main() {
+	int n, m, sx, sy;
+	while (scanf("%d %d",&n,&m)!=EOF)
+	{
+		int map[N][N], dist[N][N], vis[N][N]={{0}};
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				scanf("%d", &map[i][j]);
+				if (map[i][j] == 3) {
+					sx = i; 
+					sy = j;
+				}
+			}
+		}
+		int ans = BFS(map, vis, dist, n, m, sx, sy);
+		if (ans == -1) {
+			printf("unreachable\n");
+		}
+		else {
+			printf("%d\n", ans);
+		}
+	}
+
+	system("pause");
+	return 0;
+}
---
+++ 迷宫.cpp
@@ -0,0 +1,100 @@
+﻿#include <iostream>
+#include <queue>
+using namespace std;
+
+//@file		:迷宫
+//@author	:Lvsq
+//@time		:2022/3/11 19:14:24
+
+const int MAX = 100;
+const int INF = 0x3f3f3f3f;
+typedef pair<int, int>P;
+char shuru[MAX + 5][MAX + 5];
+int n, m;
+int sx, sy;//记录起点的位置
+int gx, gy;//记录终点的位置
+int d[MAX + 5][MAX + 5];//用数组存储起点到某一点的距离
+int dx[4] = { 1,0,-1,0 }, dy[4] = { 0,1,0,-1 };//表明每次x和y方向的位移
+void bfs()
+{
+    queue<P> que;//queue<pair<int,int>>que;
+    //初始化所有点为INF
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            d[i][j] = INF;
+    que.push(P(sx, sy));
+    d[sx][sy] = 0;
+    while (que.size())
+    {
+        P p = que.front();
+        que.pop();
+        int i;
+        for (i = 0; i < 4; i++)
+        {
+            //移动后的坐标
+            int nx = p.first + dx[i];
+            int ny = p.second + dy[i];
+            //判断是否还可以移动且没有到达过
+            if (nx >= 0 && nx < n && ny >= 0 && ny < m && shuru[nx][ny] != '#' && d[nx][ny] == INF)
+            {
+                que.push(P(nx, ny));
+                d[nx][ny] = d[p.first][p.second] + 1;
+                if (nx == gx && ny == gy)
+                    break;
+            }
+        }
+        if (i != 4)
+            break;
+    }
+}
+
+
+int main()
+{
+    cin >> n >> m;
+    //一行一行的输ru
+    for (int i = 0; i < n; i++)
+        cin >> shuru[i];
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < m; j++)
+        {
+            if (shuru[i][j] == 'S')
+            {
+                sx = i;
+                sy = j;
+            }
+            if (shuru[i][j] == 'G')
+            {
+                gx = i;
+                gy = j;//写成了i。。。这个bug找了半天。。
+            }
+        }
+    }
+    bfs();
+    cout << d[gx][gy] << endl;
+    return 0;
+}
+
+
+/*
+10 10
+#S######.#
+......#..#
+.#.##.##.#
+.#........
+##.##.####
+....#....#
+.#######.#
+....#.....
+.####.###.
+....#...G#
+*/
+/*
+5 5
+S.###
+#.##.
+#.##.
+...#.
+#.#.G
+*/   
